56d22975d87b21e98c0e7f4d9b0792fdfb6139b0
Bruce Gustin <35908618+bgustin53@users.noreply.github.com>
2025-03-24 15:03:51 -0700

    Implemented Spawn Manager

--- Assets/Scripts/Managers/SpawnManager.cs
+++ Assets/Scripts/Managers/SpawnManager.cs
@@ -4,21 +4,24 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    [SerializeField] private GameObject[] animals;
-    private float spawnRange;
+    [SerializeField] private GameObject[] animalPrefab;
+    private float zStart;
+    private float xSpawnRange;
     private float startDelay = 2;
     private float repeatRate = 1;
 
     // Start is called before the first frame update
     void Start()
     {
+        zStart = 30;
+        xSpawnRange = 20;
         InvokeRepeating("SpawnAnimal", startDelay, repeatRate);
     }
 
     private void SpawnAnimal()
     {
-        int choice = Random.Range(0, animals.Length - 1);
-        float position = Random.Range(-spawnRange, spawnRange);
-        Instantiate(animals[choice], new Vector3(0, 0, position), Quaternion.identity);
+        int choice = Random.Range(0, animalPrefab.Length);
+        float position = Random.Range(-xSpawnRange, xSpawnRange);
+        Instantiate(animalPrefab[choice], new Vector3(position, 0, zStart), Quaternion.Euler(0, 180, 0));
     }
 }
